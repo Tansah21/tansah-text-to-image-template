24a6bc948a402be106ecd18ff4b2f8a82d4731d5
IAmMorloc <kyle_syh@sina.com>
2025-08-16 22:28:50 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,23 @@
-export default {
+// src/index.ts
+var index_default = {
   async fetch(request, env) {
     const inputs = {
-      prompt: "cyberpunk cat",
+      prompt: "An energetic Naxi Cultural Experience unfolds, centered around a roaring bonfire under the night sky. People, many in traditional Naxi attire, joyfully dance in lively circles around the crackling fire, their movements rhythmic and celebratory. The atmosphere is vibrant, filled with the sounds of singing and laughter, fostering a powerful sense of warmth, belonging, and shared cultural celebration amongst all participants.,style:realistic",
+      steps: 8,
+      seed: Math.floor(Math.random() * 10)
     };
-
     const response = await env.AI.run(
-      "@cf/stabilityai/stable-diffusion-xl-base-1.0",
-      inputs,
+      "@cf/black-forest-labs/flux-1-schnell",
+      inputs
     );
-
     return new Response(response, {
       headers: {
-        "content-type": "image/png",
-      },
+        "content-type": "image/png"
+      }
     });
-  },
-} satisfies ExportedHandler<Env>;
+  }
+};
+export {
+  index_default as default
+};
+//# sourceMappingURL=index.js.map
